d39ea5bd575d54738dec1ab423e5412657835fd1
Suyeon Son <suyeon.son@icloud.com>
2016-03-04 08:42:45 +1100

    update firebase. hook up firebase to slider responses

--- js/app.js
+++ js/app.js
@@ -2,16 +2,32 @@
 
 angular.module("skin", [
 	"ngRoute",
-	"bookrx.signin",
-	"bookrx.signup",
-	"bookrx.home"
+	"firebase"
 ])
-// .
-// config(["$routeProvider", function($routeProvider) {
-// 	// set default view of app to sign in page
-// 	$routeProvider.otherwise({
-// 		redirectTo: "home"
-// 	});
+.controller('SliderCtrl', ["$scope", "$firebaseArray", function($scope, $firebaseArray) {
+	var fireObj = new Firebase("https://skin-editorslab.firebaseio.com");
 
-// 	// default view to home page w/ the calculator. to save content, log in.
-// }]);	
+	var sliderBarHeight = 5;
+	var sliderHeight = $("#slider").height() - sliderBarHeight;
+
+	$scope.responses = $firebaseArray(fireObj);;
+
+	// init jquery ui draggable to imitate slider
+	$("#slider-bar").draggable({ 
+		containment: "#slider", 
+		scroll: false,
+		drag: function(e, ui) {
+			var sliderPct = Math.round((sliderHeight-ui.position.top)/sliderHeight * 100);
+			$("#slider-pct").text(sliderPct+"%");
+			$("#slider-pct").css({
+				"top": ui.position.top - 7.5 + "px"
+			});
+		},
+		stop: function(e, ui) {
+			$("#slider-answer").fadeIn();
+
+			var sliderPct = Math.round((sliderHeight-ui.position.top)/sliderHeight * 100);
+			$scope.responses.$add(sliderPct);
+		}
+	});
+}]);
